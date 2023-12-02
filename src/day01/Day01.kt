75b8320203adbd8519c9e7a1a8d05884a04f36d1
vishnu <vismad@proton.me>
2023-12-02 13:57:10 -0600

    Day 1 & 2 solutions

---
+++ src/day01/Day01.kt
@@ -0,0 +1,56 @@
+package day01
+
+import readInput
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return input.sumOf { line ->
+            val firstDigit = line.first { it.isDigit() }
+            val lastDigit = line.last { it.isDigit() }
+
+            Integer.parseInt("$firstDigit$lastDigit")
+        }
+    }
+
+    val numberMap = mapOf(
+        "one" to '1',
+        "two" to '2',
+        "three" to '3',
+        "four" to '4',
+        "five" to '5',
+        "six" to '6',
+        "seven" to '7',
+        "eight" to '8',
+        "nine" to '9'
+    )
+
+    fun part2(input: List<String>): Int {
+        val searchValues = numberMap.values
+            .map { it.toString() }
+            .plus(numberMap.keys)
+
+        return input.sumOf { line ->
+            val (_, firstDigitOrString) = line.findAnyOf(searchValues)!!
+            val (_, lastDigitOrString) = line.findLastAnyOf(searchValues)!!
+
+            val firstDigit = if (firstDigitOrString[0].isDigit()) {
+                firstDigitOrString[0]
+            } else {
+                numberMap[firstDigitOrString]
+            }
+
+            val lastDigit = if (lastDigitOrString[0].isDigit()) {
+                lastDigitOrString[0]
+            } else {
+                numberMap[lastDigitOrString]
+            }
+
+            Integer.parseInt("$firstDigit$lastDigit")
+        }
+    }
+
+    val input = readInput("day01/input")
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
+}
---
+++ src/day02/Day02.kt
@@ -0,0 +1,80 @@
+package day02
+
+import readInput
+
+fun main() {
+
+    data class Round(val r: Int, val g: Int, val b: Int)
+
+    data class Game(val id: Int, val rounds: List<Round>) {
+        val power: Int
+            get() {
+                var minRed = 0
+                var minGreen = 0
+                var minBlue = 0
+
+                rounds.forEach {
+                    if (it.r > minRed) minRed = it.r
+                    if (it.g > minGreen) minGreen = it.g
+                    if (it.b > minBlue) minBlue = it.b
+                }
+
+                return minRed * minGreen * minBlue
+            }
+    }
+
+    fun parseGame(id: Int, line: String): Game {
+        val rounds = line
+            .substringAfter(": ")
+            .split(';')
+            .map { round ->
+                var rCount = 0
+                var gCount = 0
+                var bCount = 0
+
+                round.split(',')
+                    .map { it.trim().split(' ') }
+                    .forEach {
+                        val count = Integer.parseInt(it[0])
+                        when (it[1]) {
+                            "red" -> rCount += count
+                            "green" -> gCount += count
+                            "blue" -> bCount += count
+                        }
+                    }
+
+                Round(rCount, gCount, bCount)
+            }
+
+        return Game(id, rounds)
+    }
+
+    fun part1(input: List<String>): Int {
+        val maxRedCubeCount = 12
+        val maxGreenCubeCount = 13
+        val maxBlueCubeCount = 14
+
+        val games = input.mapIndexed { index, line ->
+            parseGame(index + 1, line)
+        }
+
+        return games.sumOf { game ->
+            val hasInvalidRound = game.rounds.any {
+                it.r > maxRedCubeCount || it.g > maxGreenCubeCount || it.b > maxBlueCubeCount
+            }
+            if (hasInvalidRound) 0 else game.id
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        val games = input.mapIndexed { index, line ->
+            parseGame(index + 1, line)
+        }
+
+        return games.sumOf { it.power }
+    }
+
+    val input = readInput("day02/input")
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
+}
